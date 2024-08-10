c22a8b44fb22a700e47d048c507ff66aebea0e63
Erick-Pereira <erickbaptistapereira@gmail.com>
2024-08-09 21:02:39 -0300

    eu ja não sei mais o que esta acontecendo, eu não to conseguindo puxar o tamanho da array e eu não to conseguindo pegar o elemento pelo id

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,50 @@
+const localStorageKey = 'to-do-list-gn'
 function addTask() {
-    var task = document.getElementById("Task").value;
-    taskList.push(task);
-      var b = "<div name=tasks id=tasks><input type=checkbox onClick=checou()><p>"+task+"</p><button onClick=deleteTask()>Delete</button></div>"
-      var c = document.getElementById("toDoList");
-      c.innerHTML = c.innerHTML + b;
-    document.getElementById("Task").value = "";
-    console.log(taskList);
-  }
-  var taskList = []
+    let input = document.getElementById('Input-Task')
 
+
+    if(!input.value){
+      alert('Digite algo para inserir em sua lista');
+    }
+    else{
+      let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
+      let lenght = values.lenght
+      if(lenght == null){
+        lenght=0;
+      }
+      console.log(lenght)
+      console.log(values)
+      console.log(values.lenght);
+      lenght++;
+      values.push({
+        id: lenght,
+        name: input.value,
+        finished: false
+      })
+      console.log(values)
+      localStorage.setItem(localStorageKey,JSON.stringify(values));
+      showValues();
+    }
+  }
   function deleteTask(){
     console.log("Eu não sei oq eu vou fazer aqui")
   }
   function checou(){
     console.log("aaaaaaaaa")
+  }
+
+  function showValues(){
+    let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
+    let list = document.getElementById('toDoList')
+    list.innerHTML= ''
+    for(let i = 0; i<values.lenght; i++)
+      {
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onClick='removeItem()'></button></li>`
+      }
+  }
+  showValues();
+
+  function removeItem(data){
+    console.log(data)
+    let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
   }
