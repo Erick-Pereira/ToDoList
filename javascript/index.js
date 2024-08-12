7c4e141b63d52894533de507365c193823558a17
Erick-Pereira <erickbaptistapereira@gmail.com>
2024-08-12 20:49:48 -0300

    correção javascript e arrumei o checked

--- javascript/index.js
+++ javascript/index.js
@@ -1,50 +1,58 @@
 const localStorageKey = 'to-do-list-gn'
 function addTask() {
-    let input = document.getElementById('Input-Task')
-
-
-    if(!input.value){
-      alert('Digite algo para inserir em sua lista');
+  let input = document.getElementById('Input-Task')
+  if (!input.value) {
+    alert('Digite algo para inserir em sua lista');
+  }
+  else {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let length = values.length
+    if (length == null) {
+      length = 0;
     }
-    else{
-      let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
-      let lenght = values.lenght
-      if(lenght == null){
-        lenght=0;
-      }
-      console.log(lenght)
-      console.log(values)
-      console.log(values.lenght);
-      lenght++;
-      values.push({
-        id: lenght,
-        name: input.value,
-        finished: false
-      })
-      console.log(values)
-      localStorage.setItem(localStorageKey,JSON.stringify(values));
-      showValues();
+    length++;
+    values.push({
+      id: length,
+      name: input.value,
+      finished: false
+    })
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+    showValues();
+  }
+}
+
+function showValues() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let list = document.getElementById('toDoList')
+  list.innerHTML = ''
+  for (let i = 0; i < values.length; i++) {
+    let comp = `${values[i].name}`
+    let check = '';
+    if (values[i].finished) {
+      comp = `<del>${values[i].name}</del>`
+      check = 'checked';
     }
+    list.innerHTML += `<li><input type="checkbox" ${check} onClick="handleCheckboxChange(${values[i].id})"><span style={{ marginLeft: "10px" }}> 
+                        ${comp}
+                        </span> <button id='btn-delete' onClick='removeItem(${values[i].id})'></button></li>`
   }
-  function deleteTask(){
-    console.log("Eu não sei oq eu vou fazer aqui")
-  }
-  function checou(){
-    console.log("aaaaaaaaa")
-  }
+}
+showValues();
 
-  function showValues(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
-    let list = document.getElementById('toDoList')
-    list.innerHTML= ''
-    for(let i = 0; i<values.lenght; i++)
-      {
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onClick='removeItem()'></button></li>`
-      }
-  }
-  showValues();
+function removeItem(data) {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let index = values.findIndex(x => x.id == data)
+  values.splice(index, 1)
+  localStorage.setItem(localStorageKey, JSON.stringify(values))
+  showValues()
+}
 
-  function removeItem(data){
-    console.log(data)
-    let values = JSON.parse(localStorage.getItem(localStorageKey)|| "[]")
-  }
+function handleCheckboxChange(data) {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let index = values.findIndex(x => x.id == data)
+  let value = values[index]
+  value.finished = !value.finished
+  values[index] = value;
+  localStorage.setItem(localStorageKey, JSON.stringify(values))
+  showValues()
+}
